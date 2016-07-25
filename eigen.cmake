e408bc0e36b85fe5e19d6fd327eb77f86d09e975
Connor Weeks <cw@bluespiral.io>
2016-07-25 20:54:10 +0100

    Improved install locations to reduce clutter

--- eigen.cmake
+++ eigen.cmake
@@ -1,17 +1,17 @@
 include(ExternalProject)
 include(eigen_VERSION)
 
-set(eigen_INSTALL ${PROJECT_SOURCE_DIR}/external/eigen/eigen-eigen-${eigen_archive_hash})
+set(eigen_INSTALL ${PROJECT_SOURCE_DIR}/external/include/eigen/eigen-eigen-${eigen_archive_hash})
 
 set(eigen_INCLUDE_DIRS
-        ${PROJECT_SOURCE_DIR}/external/eigen
+        ${PROJECT_SOURCE_DIR}/external/include/eigen
         ${eigen_INSTALL})
 
 ExternalProject_Add(eigen
-        PREFIX ${PROJECT_SOURCE_DIR}/external/eigen
+        PREFIX ${PROJECT_SOURCE_DIR}/external/src/eigen
         URL ${eigen_URL}
         URL_HASH ${eigen_HASH}
-        DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
+        DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
         
         CMAKE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=Release
--- eigen.sh
+++ eigen.sh
@@ -79,7 +79,7 @@ if [ "${MODE}" == "external" ]; then
     echo "set(eigen_archive_hash ${EIGEN_ARCHIVE_HASH})" >> ${EIGEN_OUT}
     echo "set(eigen_HASH SHA256=${EIGEN_HASH})" >> ${EIGEN_OUT}
     echo "set(eigen_dir eigen-eigen-${EIGEN_ARCHIVE_HASH})" >> ${EIGEN_OUT}
-    cp ${SCRIPT_DIR}/../cmake/eigen.cmake ${CMAKE_DIR}
+    cp ${SCRIPT_DIR}/eigen.cmake ${CMAKE_DIR}
     echo "Wrote eigen_VERSION.cmake and eigen.cmake to ${CMAKE_DIR}"
 elif [ "${MODE}" == "install" ]; then
     # copy eigen files to cmake directory
