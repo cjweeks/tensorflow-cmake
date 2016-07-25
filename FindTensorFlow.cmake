d7e85b5764a14ee35434508734d0d3ea6d36dd9e
Connor Weeks <cw@bluespiral.io>
2016-07-25 20:53:22 +0100

    Fixed incorrect variables

--- FindTensorFlow.cmake
+++ FindTensorFlow.cmake
@@ -2,9 +2,10 @@
 # Connor Weeks
 # Locates the tensorFlow include directories and library.
 
+include(FindPackageHandleStandardArgs)
 unset(TensorFlow_FOUND)
 
-find_path(TensorFlow_INCLUDE_DIRS
+find_path(TensorFlow_INCLUDE_DIR
         NAMES
         tensorflow/core
         tensorflow/cc
@@ -20,6 +21,15 @@ find_library(TensorFlow_LIBRARY NAMES tensorflow_all
 
 set(TensorFlow_LIBS ${TensorFlow_LIBRARY})
 
-if(TesorFlow_LIBS AND TensorFlow_INCLUDE_DIRS)
-    set(TensorFlow_FOUND 1)
+
+# handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(TensorFlow DEFAULT_MSG TensorFlow_INCLUDE_DIR TensorFlow_LIBRARY)
+
+if (TensorFlow_FOUND)
+    set(TensorFlow_LIBRARIES ${TensorFlow_LIBRARY} )
+    set(TensorFlow_INCLUDE_DIRS ${TensorFlow_INCLUDE_DIR} )
+    set(TensorFlow_DEFINITIONS )
 endif()
+
+mark_as_advanced(TensorFlow_INCLUDE_DIR TensorFlow_LIBRARY)
