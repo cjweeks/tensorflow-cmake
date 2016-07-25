f77dd44d18e1b0df47b8765cfe356c9c7e9f542d
Connor Weeks <cw@bluespiral.io>
2016-07-25 14:57:22 +0100

    Simplified includes and install locations

--- eigen.cmake
+++ eigen.cmake
@@ -1,27 +1,22 @@
 include(ExternalProject)
 include(eigen_VERSION)
 
+set(eigen_INSTALL ${PROJECT_SOURCE_DIR}/external/eigen/eigen-eigen-${eigen_archive_hash})
+
 set(eigen_INCLUDE_DIRS
-        ${PROJECT_SOURCE_DIR}/external/eigen-archive
-        ${PROJECT_SOURCE_DIR}/external/eigen-archive/${eigen_dir}
-        ${PROJECT_SOURCE_DIR}/external/third_party
-        ${PROJECT_SOURCE_DIR}/external)
+        ${PROJECT_SOURCE_DIR}/external/eigen
+        ${eigen_INSTALL})
 
-set(eigen_BUILD ${PROJECT_SOURCE_DIR}/external/third_party/eigen/src/eigen)
-set(eigen_INSTALL ${PROJECT_SOURCE_DIR}/external/third_party/eigen/install)
-
-ExternalProject_Add(protobuf
-        PREFIX ${PROJECT_SOURCE_DIR}/external/protobuf
+ExternalProject_Add(eigen
+        PREFIX ${PROJECT_SOURCE_DIR}/external/eigen
         URL ${eigen_URL}
         URL_HASH ${eigen_HASH}
         DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
-        INSTALL_DIR "${eigen_INSTALL}"
-        BINARY_DIR "${eigen_INSTALL}"
-
+        
         CMAKE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=Release
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
         -DCMAKE_INSTALL_PREFIX:STRING=${eigen_INSTALL}
-        -DINCLUDE_INSTALL_DIR:STRING=${PROJECT_SOURCE_DIR}/external/eigen-archive/${eigen_dir}
-
+        -DINCLUDE_INSTALL_DIR:STRING=${eigen_INSTALL})
+      
 include_directories(${eigen_INCLUDE_DIRS})
