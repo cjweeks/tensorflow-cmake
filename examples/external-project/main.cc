213ade8fa177f592d842180945f65380c333f67a
Connor Weeks <cw@bluespiral.io>
2016-07-25 23:03:27 +0100

    Added function to check status

--- examples/external-project/main.cc
+++ examples/external-project/main.cc
@@ -1,29 +1,31 @@
 #include "tensorflow/core/public/session.h"
 #include "tensorflow/core/platform/env.h"
 
+/**
+ * Checks if the given status is ok.
+ * If not, the status is printed and the
+ * program is terminated.
+ */
+void checkStatus(const tensorflow::Status& status) {
+  if (!status.ok()) {
+    std::cout << status.ToString() << std::endl;
+    exit(1);
+  }
+}
 
 int main(int argc, char** argv) {
     namespace tf = tensorflow;
 
     tf::Session* session;
     tf::Status status = tf::NewSession(tf::SessionOptions(), &session);
-    if (!status.ok()) {
-        std::cout << status.ToString() << std::endl;
-        return 1;
-    }
+    checkStatus(status);
 
     tf::GraphDef graph_def;
     status = ReadBinaryProto(tf::Env::Default(), "graph.pb", &graph_def);
-    if (!status.ok()) {
-        std::cout << status.ToString() << std::endl;
-        return 1;
-    }
+    checkStatus(status);
 
     status = session->Create(graph_def);
-    if (!status.ok()) {
-        std::cout << status.ToString() << std::endl;
-        return 1;
-    }
+    checkStatus(status);
 
     tf::Tensor x(tf::DT_FLOAT, tf::TensorShape()), y(tf::DT_FLOAT, tf::TensorShape());
     x.scalar<float>()() = 23.0;
@@ -33,10 +35,7 @@ int main(int argc, char** argv) {
     std::vector<tf::Tensor> output_tensors;
 
     status = session->Run(input_tensors, {"z"}, {}, &output_tensors);
-    if (!status.ok()) {
-        std::cout << status.ToString() << std::endl;
-        return 1;
-    }
+    checkStatus(status);
     
     tf::Tensor output = output_tensors[0];
     std::cout << "Success: " << output.scalar<float>() << "!" << std::endl;
