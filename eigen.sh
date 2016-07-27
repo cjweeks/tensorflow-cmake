fb5e33b24a77c3d775d719409fd739846711eda0
Connor Weeks <cw@bluespiral.io>
2016-07-27 04:26:45 +0100

    Major changes to script control flow

--- eigen.sh
+++ eigen.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
-# Exits if user doe not have root priveledges
-check_root () {
-    if [ "${EUID}" -ne 0 ]; then
-	echo "Please run as root to install."
-	exit 1
-    fi
+# Prints an error message and exits with an error code of 1
+fail () {
+    echo "Command failed; script terminated"
+    exit 1
 }
 
 # Prints usage information concerning this script
 print_usage () {
-    echo "Usage: ${0} external|install <tensorflow-source-dir> [cmake-dir]"
+    echo "|
+| Usage: ${0} generate|install [args]
+|
+| --> ${0} generate installed|external <tensorflow-source-dir> [<cmake-dir>]:
+|
+|     Generates the cmake files for the given installation of tensorflow
+|     and writes them to <cmake-dir>
+|
+| --> ${0} install <tensorflow-source-dir> [<install-dir> <download-dir>]
+|
+|     Downloads Eigen to <donload-dir> (defaults to the current directory),
+|     and installs it to <instal-dir> (defaults to /usr/local).
+|"
 }
 
 # validate and assign input
@@ -21,20 +31,38 @@ if [ "$#" -lt 2 ]; then
 fi
 # Determine mode
 if [ "${1}" == "install" ]; then
-    check_root
     MODE="install"
-elif [ "${1}" == "external" ]; then
-    MODE="external"
+elif [ "${1}" == "generate" ]; then
+    MODE="generate"
 else
     print_usage
     exit 1
 fi
+
 # get arguments
-TF_DIR="${2}"
-CMAKE_DIR="."
-if [ "$#" -gt 2 ]; then
-    CMAKE_DIR="${3}"
+if [ "${MODE}" == "install" ]; then
+    TF_DIR="${2}"
+    INSTALL_DIR="/usr/local"
+    DOWNLOAD_DIR="."
+    if [ "$#" -gt 2 ]; then
+       INSTALL_DIR="${3}"
+    fi
+    if [ "$#" -gt 3 ]; then
+	DOWNLOAD_DIR="${4}"
+    fi
+elif [ "${MODE}" == "generate" ]; then
+    GENERATE_MODE="${2}"
+    if [ "${GENERATE_MODE}" != "installed" ] && [ "${GENERATE_MODE}" != "external" ]; then
+	print_usage
+	exit 1
+    fi
+    TF_DIR="${3}"
+    CMAKE_DIR="."
+    if [ "$#" -gt 3 ]; then
+	CMAKE_DIR="${4}"
+    fi
 fi
+    
 
 # locate eigen archive in tensorflow directory
 ANY="[^\)]*"
@@ -54,11 +82,11 @@ HASH="s/\s*sha256${QUOTE_START}\(${ANY_HEX}\)${QUOTE_END}/\1/p"
 ARCHIVE_HASH="s=.*/\(${ANY_HEX}\)\\.tar\\.gz=\1=p"
 
 echo "Finding eigen version in ${TF_DIR}..."
-EIGEN_TEXT=$(grep -Pzro ${EIGEN_REGEX} ${TF_DIR})
+EIGEN_TEXT=$(grep -Pzro ${EIGEN_REGEX} ${TF_DIR}) || fail
 
-EIGEN_URL=$(echo "${EIGEN_TEXT}" | sed -n ${URL})
-EIGEN_HASH=$(echo "${EIGEN_TEXT}" | sed -n ${HASH})
-EIGEN_ARCHIVE_HASH=$(echo "${EIGEN_URL}" | sed -n ${ARCHIVE_HASH})
+EIGEN_URL=$(echo "${EIGEN_TEXT}" | sed -n ${URL}) || fail
+EIGEN_HASH=$(echo "${EIGEN_TEXT}" | sed -n ${HASH}) || fail
+EIGEN_ARCHIVE_HASH=$(echo "${EIGEN_URL}" | sed -n ${ARCHIVE_HASH}) || fail
 
 if [ -z "${EIGEN_URL}" ] || [ -z "${EIGEN_HASH}" ] || [ -z "${EIGEN_ARCHIVE_HASH}" ]; then
     echo "Failure: Could not find all required strings in ${TF_DIR}"
@@ -72,28 +100,45 @@ echo "Eigen URL Hash:      ${EIGEN_HASH}"
 echo "Eigen Archive Hash:  ${EIGEN_ARCHIVE_HASH}"
 echo
 
-if [ "${MODE}" == "external" ]; then
-    # output Eigen information to file
-    EIGEN_OUT="${CMAKE_DIR}/Eigen_VERSION.cmake"
-    echo "set(Eigen_URL ${EIGEN_URL})" > ${EIGEN_OUT}
-    echo "set(Eigen_ARCHIVE_HASH ${EIGEN_ARCHIVE_HASH})" >> ${EIGEN_OUT}
-    echo "set(Eigen_HASH SHA256=${EIGEN_HASH})" >> ${EIGEN_OUT}
-    echo "set(Eigen_DIR eigen-eigen-${EIGEN_ARCHIVE_HASH})" >> ${EIGEN_OUT}
-    cp ${SCRIPT_DIR}/Eigen.cmake ${CMAKE_DIR}
-    echo "Wrote Eigen_VERSION.cmake and Eigen.cmake to ${CMAKE_DIR}"
-elif [ "${MODE}" == "install" ]; then
-    # copy eigen files to cmake directory
-    cp -p ${SCRIPT_DIR}/FindEigen.cmake ${CMAKE_DIR}
-    # donwload eigen and extract to /usr/local/include
-    mkdir -p /usr/local/include/eigen
-    rm -r /usr/local/include/eigen/*
-    cd /usr/local/include/eigen
-    wget ${EIGEN_URL}
-    tar -zxvf *
-    echo
-    echo "All Eigen files copied to /usr/local/eigen"
-    echo "FindEigen.cmake copied to ${CMAKE_DIR}"
+# perform requested action
+if [ "${MODE}" == "install" ]; then
+    # donwload eigen and extract to download directory
+    echo "Downlaoding Eigen to ${DOWNLOAD_DIR}"
+    cd ${DOWNLOAD_DIR} || fail
+    rm -rf eigen-eigen-${EIGEN_ARCHIVE_HASH} || fail
+    rm -f ${EIGEN_ARCHIVE_HASH}.tar.gz* || fail
+    wget ${EIGEN_URL} || fail
+    tar -zxvf ${EIGEN_ARCHIVE_HASH}.tar.gz || fail
+    cd eigen-eigen-${EIGEN_ARCHIVE_HASH} || fail
+    # create build directory and build
+    mkdir build || fail
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DINCLUDE_INSTALL_DIR=include/eigen-eigen-${EIGEN_ARCHIVE_HASH} .. || fail
+    make || fail
+    make install || fail
+    echo "Installation complete"
+    echo "Cleaning up..."
+    # clean up
+    cd ../..
+    rm -rf eigen-eigen-${EIGEN_ARCHIVE_HASH} || fail
+    rm -f ${EIGEN_ARCHIVE_HASH}.tar.gz* || fail
+elif [ "${MODE}" == "generate" ]; then
+    if [ "${GENERATE_MODE}" == "external" ]; then
+	# output Eigen information to file
+	EIGEN_OUT="${CMAKE_DIR}/Eigen_VERSION.cmake"
+	echo "set(Eigen_URL ${EIGEN_URL})" > ${EIGEN_OUT} || fail
+	echo "set(Eigen_ARCHIVE_HASH ${EIGEN_ARCHIVE_HASH})" >> ${EIGEN_OUT} || fail
+	echo "set(Eigen_HASH SHA256=${EIGEN_HASH})" >> ${EIGEN_OUT} || fail
+	echo "set(Eigen_DIR eigen-eigen-${EIGEN_ARCHIVE_HASH})" >> ${EIGEN_OUT} || fail
+	cp ${SCRIPT_DIR}/Eigen.cmake ${CMAKE_DIR} || fail
+	echo "Wrote Eigen_VERSION.cmake and Eigen.cmake to ${CMAKE_DIR}"
+    elif [ "${GENERATE_MODE}" == "installed" ]; then
+	# copy eigen file to cmake directory
+	cp ${SCRIPT_DIR}/FindEigen.cmake ${CMAKE_DIR} || fail
+	echo "FindEigen.cmake copied to ${CMAKE_DIR}"
+    fi
 fi
+
 
 echo "Done"
 
--- protobuf.sh
+++ protobuf.sh
@@ -2,18 +2,29 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
-# Exits if user has  root priveledges and exits if not
-check_root () {
-    if [ "${EUID}" -ne 0 ]; then
-	echo "Please run as root to install."
-	exit 1
-    fi
+# Prints an error message and exits with an error code of 1
+fail () {
+    echo "Command failed; script terminated"
+    exit 1
 }
 
 # Prints usage information concerning this script
 print_usage () {
-    echo "Usage: ${0} external|install <tensorflow-source-dir> [cmake-dir]"
+    echo "|
+| Usage: ${0} generate|install [args]
+|
+| --> ${0} generate installed|external <tensorflow-source-dir> [<cmake-dir>]:
+|
+|     Generates the cmake files for the given installation of tensorflow
+|     and writes them to <cmake-dir>
+|
+| --> ${0} install <tensorflow-source-dir> [<install-dir> <download-dir>]
+|
+|     Downloads Protobuf to <donload-dir> (defaults to the current directory),
+|     and installs it to <instal-dir> (defaults to /usr/local).
+|"
 }
+
 
 # validate and assign input
 if [ "$#" -lt 2 ]; then
@@ -22,19 +33,36 @@ if [ "$#" -lt 2 ]; then
 fi
 # Determine mode
 if [ "${1}" == "install" ]; then
-    check_root
     MODE="install"
-elif [ "${1}" == "external" ]; then
-    MODE="external"
+elif [ "${1}" == "generate" ]; then
+    MODE="generate"
 else
     print_usage
     exit 1
 fi
+
 # get arguments
-TF_DIR="${2}"
-CMAKE_DIR="."
-if [ "$#" -gt 2 ]; then
-    CMAKE_DIR="${3}"
+if [ "${MODE}" == "install" ]; then
+    TF_DIR="${2}"
+    INSTALL_DIR="/usr/local"
+    DOWNLOAD_DIR="."
+    if [ "$#" -gt 2 ]; then
+       INSTALL_DIR="${3}"
+    fi
+    if [ "$#" -gt 3 ]; then
+	DOWNLOAD_DIR="${4}"
+    fi
+elif [ "${MODE}" == "generate" ]; then
+    GENERATE_MODE="${2}"
+    if [ "${GENERATE_MODE}" != "installed" ] && [ "${GENERATE_MODE}" != "external" ]; then
+	print_usage
+	exit 1
+    fi
+    TF_DIR="${3}"
+    CMAKE_DIR="."
+    if [ "$#" -gt 3 ]; then
+	CMAKE_DIR="${4}"
+    fi
 fi
 
 # locate protobuf information in tensorflow directory
@@ -54,10 +82,10 @@ COMMIT="s/\s*commit${QUOTE_START}\(${ANY_HEX}\)${QUOTE_END}/\1/p"
 REMOTE="s/\s*remote${QUOTE_START}\(${ANY_NO_QUOTES}\)${QUOTE_END}/\1/p"
 
 echo "Finding protobuf information in ${TF_DIR}..."
-PROTOBUF_TEXT=$(grep -Pzro ${PROTOBUF_REGEX} ${TF_DIR})
+PROTOBUF_TEXT=$(grep -Pzro ${PROTOBUF_REGEX} ${TF_DIR}) || fail
 
-PROTOBUF_COMMIT=$(echo "${PROTOBUF_TEXT}" | sed -n ${COMMIT})
-PROTOBUF_URL=$(echo "${PROTOBUF_TEXT}" | sed -n ${REMOTE})
+PROTOBUF_COMMIT=$(echo "${PROTOBUF_TEXT}" | sed -n ${COMMIT}) || fail
+PROTOBUF_URL=$(echo "${PROTOBUF_TEXT}" | sed -n ${REMOTE}) || fail
 
 if [ -z "${PROTOBUF_URL}" ] || [ -z "${PROTOBUF_COMMIT}" ]; then
     echo "Failure: Could not find all required strings in ${TF_DIR}"
@@ -66,29 +94,36 @@ fi
 
 # print information
 echo
-echo "Protobuf Repo:       ${PROTOBUF_URL}"
-echo "Protobuf Commit:     ${PROTOBUF_COMMIT}"
+echo "Protobuf Repository:  ${PROTOBUF_URL}"
+echo "Protobuf Commit:      ${PROTOBUF_COMMIT}"
 echo
 
-if [ "${MODE}" == "external" ]; then
-    # output protobuf information to file
-    PROTOBUF_OUT="${CMAKE_DIR}/Protobuf_VERSION.cmake"
-    echo "set(Protobuf_URL ${PROTOBUF_URL})" > ${PROTOBUF_OUT}
-    echo "set(Protobuf_COMMIT ${PROTOBUF_COMMIT})" >> ${PROTOBUF_OUT}
-    cp ${SCRIPT_DIR}/Protobuf.cmake ${CMAKE_DIR}
-    echo "Wrote Protobuf_VERSION.cmake and Protobuf.cmake to ${CMAKE_DIR}"
-elif [ "${MODE}" == "install" ]; then
+# perform requested action
+if [ "${MODE}" == "install" ]; then
     # clone protobuf from its git repository
-    git clone ${PROTOBUF_URL}
-    git reset --hard ${PROTOBUF_COMMIT}
-    cd protobuf
-    ./autogen.sh
-    ./configure
-    make
-    make check
-    sudo make install
-    sudo ldconfig
-    echo "Protobuf has been installed to /usr/local"
+    cd ${DOWNLOAD_DIR} || fail
+    git clone ${PROTOBUF_URL} || fail
+    git reset --hard ${PROTOBUF_COMMIT} || fail
+    # configure
+    cd protobuf || fail
+    ./autogen.sh || fail
+    ./configure --prefix=${INSTALL_DIR} || fail
+    # build and install
+    make || fail
+    make check || fail
+    sudo make install || fail
+    sudo ldconfig || fail
+    echo "Protobuf has been installed to ${INSTALL_DIR}"
+elif [ "${MODE}" == "generate" ]; then
+    if [ "${GENERATE_MODE}" == "external" ]; then
+	# output protobuf information to file
+	PROTOBUF_OUT="${CMAKE_DIR}/Protobuf_VERSION.cmake"
+	echo "set(Protobuf_URL ${PROTOBUF_URL})" > ${PROTOBUF_OUT} || fail
+	echo "set(Protobuf_COMMIT ${PROTOBUF_COMMIT})" >> ${PROTOBUF_OUT} || fail
+	cp ${SCRIPT_DIR}/Protobuf.cmake ${CMAKE_DIR} || fail
+	echo "Wrote Protobuf_VERSION.cmake and Protobuf.cmake to ${CMAKE_DIR}"
+    elif [ "${GENERATE_MODE}" == "installled" ]; then
+	echo "No files to generate (use CMake's built-in FindProtobuf module)"
+    fi
 fi
-
 echo "Done"
