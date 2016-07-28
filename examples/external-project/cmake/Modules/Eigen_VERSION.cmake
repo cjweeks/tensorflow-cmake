87d16b5716ecc0c17f724fbafbe1313021885c30
Connor Weeks <cweeks314@gmail.com>
2016-07-28 20:20:52 +0100

    Removed generated example files

--- examples/external-project/cmake/Modules/Eigen_VERSION.cmake
+++
@@ -1,4 +0,0 @@
-set(Eigen_URL https://bitbucket.org/eigen/eigen/get/b4fa9622b809.tar.gz)
-set(Eigen_ARCHIVE_HASH b4fa9622b809)
-set(Eigen_HASH SHA256=2862840c2de9c0473a4ef20f8678949ae89ab25965352ee53329e63ba46cec62)
-set(Eigen_DIR eigen-eigen-b4fa9622b809)
--- examples/external-project/cmake/Modules/Protobuf_VERSION.cmake
+++
@@ -1,2 +0,0 @@
-set(Protobuf_URL https://github.com/google/protobuf)
-set(Protobuf_COMMIT ed87c1fe2c6e1633cadb62cf54b2723b2b25c280)
