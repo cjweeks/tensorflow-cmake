61a40ea40140b6fa71857317b66fc30073558e54
Connor Weeks <cweeks314@gmail.com>
2016-07-30 17:51:23 +0100

    Formatting fixes for consistency

--- FindEigen.cmake
+++ FindEigen.cmake
@@ -7,20 +7,21 @@ include(Eigen_VERSION)
 unset(EIGEN_FOUND)
 
 find_path(Eigen_INCLUDE_DIR
-		NAMES
-		${Eigen_DIR}
-		${Eigen_DIR}/unsupported
-		${Eigen_DIR}/Eigen
-		HINTS
-		${Eigen_INSTALL_DIR}/include/eigen)
+        NAMES
+        ${Eigen_DIR}
+        ${Eigen_DIR}/unsupported
+        ${Eigen_DIR}/Eigen
+        HINTS
+        ${Eigen_INSTALL_DIR}/include/eigen)
 
 # set Eigen_FOUND
 find_package_handle_standard_args(Eigen DEFAULT_MSG Eigen_INCLUDE_DIR)
 
 # set external variables for usage in CMakeLists.txt
-if (EIGEN_FOUND)
-	set(Eigen_INCLUDE_DIRS ${Eigen_INCLUDE_DIR} ${Eigen_INCLUDE_DIR}/${Eigen_DIR})
+if(EIGEN_FOUND)
+    set(Eigen_INCLUDE_DIRS ${Eigen_INCLUDE_DIR} ${Eigen_INCLUDE_DIR}/${Eigen_DIR})
 endif()
+
 
 
 # hide locals from GUI
--- FindProtobuf.cmake
+++ FindProtobuf.cmake
@@ -3,22 +3,23 @@ include(Protobuf_VERSION)
 unset(PROTOBUF_FOUND)
 
 find_path(Protobuf_INCLUDE_DIR
-		NAMES
-		protobuf
-		HINTS
-		${Protobuf_INSTALL_DIR}/include/google)
+        NAMES
+        protobuf
+        HINTS
+        ${Protobuf_INSTALL_DIR}/include/google)
 
 find_library(Protobuf_LIBRARY NAMES protobuf
-		HINTS
-		${Protobuf_INSTALL_DIR}/lib)
+        HINTS
+        ${Protobuf_INSTALL_DIR}/lib)
 # set Protobuf_FOUND
 find_package_handle_standard_args(Protobuf DEFAULT_MSG Protobuf_INCLUDE_DIR Protobuf_LIBRARY)
 
 # set external variables for usage in CMakeLists.txt
-if (PROTOBUF_FOUND)
-	set(Protobuf_LIBRARIES ${Protobuf_LIBRARY})
-	set(Protobuf_INCLUDE_DIRS ${Protobuf_INCLUDE_DIR})
+if(PROTOBUF_FOUND)
+    set(Protobuf_LIBRARIES ${Protobuf_LIBRARY})
+    set(Protobuf_INCLUDE_DIRS ${Protobuf_INCLUDE_DIR})
 endif()
+
 
 
 # hide locals from GUI
