e6baac33e44a299ff06f50e51f4c396553427711
Connor Weeks <cweeks314@gmail.com>
2016-07-30 17:54:47 +0100

    Copied changed files to example projects

--- examples/install-project/cmake/Modules/FindTensorFlow.cmake
+++ examples/install-project/cmake/Modules/FindTensorFlow.cmake
@@ -21,7 +21,7 @@ find_library(TensorFlow_LIBRARY NAMES tensorflow_all
 find_package_handle_standard_args(TensorFlow DEFAULT_MSG TensorFlow_INCLUDE_DIR TensorFlow_LIBRARY)
 
 # set external variables for usage in CMakeLists.txt
-if (TENSORFLOW_FOUND)
+if(TENSORFLOW_FOUND)
     set(TensorFlow_LIBRARIES ${TensorFlow_LIBRARY})
     set(TensorFlow_INCLUDE_DIRS ${TensorFlow_INCLUDE_DIR})
 endif()
